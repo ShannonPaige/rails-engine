32ad5b10d9a21ace4d8c2117864eacd281deb6bd
ShannonPaige <spaige@gmail.com>
2016-01-06 17:51:38 -0700

    Price accuracy added

--- app/serializers/invoice_item_serializer.rb
+++ app/serializers/invoice_item_serializer.rb
@@ -1,8 +1,8 @@
 class InvoiceItemSerializer < ActiveModel::Serializer
   attributes :id, :unit_price, :created_at, :updated_at, :item_id, :invoice_id, :quantity
 
-  # def unit_price
-  #   object.unit_price
-  # end
+  def unit_price
+    sprintf( "%0.02f", object.unit_price)
+  end
 
 end
